32d9d42a695ff7dcbbb2b3142a2e4cdec2af6502
Lurker00 <Lurker00@users.noreply.github.com>
2019-07-24 09:42:46 +0300

    SysTrayDemo from https://www.codeproject.com/Articles/18783/Example-of-a-SysTray-App-in-Win32

---
+++ src/SysTrayDemo/SysTrayDemo.cpp
@@ -0,0 +1,256 @@
+// SysTrayDemo.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "SysTrayDemo.h"
+
+#define MAX_LOADSTRING 100
+#define	WM_USER_SHELLICON WM_USER + 1
+
+// Global Variables:
+HINSTANCE hInst;	// current instance
+NOTIFYICONDATA nidApp;
+HMENU hPopMenu;
+TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+TCHAR szApplicationToolTip[MAX_LOADSTRING];	    // the main window class name
+BOOL bDisable = FALSE;							// keep application state
+
+// Forward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: Place code here.
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// Initialize global strings
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_SYSTRAYDEMO, szWindowClass, MAX_LOADSTRING);
+	
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SYSTRAYDEMO));
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+//  COMMENTS:
+//
+//    This function and its usage are only necessary if you want this code
+//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
+//    function that was added to Windows 95. It is important to call this function
+//    so that the application will get 'well formed' small icons associated
+//    with it.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SYSTRAYDEMO));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_SYSTRAYDEMO);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+   HICON hMainIcon;
+
+   hInst = hInstance; // Store instance handle in our global variable
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   hMainIcon = LoadIcon(hInstance,(LPCTSTR)MAKEINTRESOURCE(IDI_SYSTRAYDEMO)); 
+
+   nidApp.cbSize = sizeof(NOTIFYICONDATA); // sizeof the struct in bytes 
+   nidApp.hWnd = (HWND) hWnd;              //handle of the window which will process this app. messages 
+   nidApp.uID = IDI_SYSTRAYDEMO;           //ID of the icon that willl appear in the system tray 
+   nidApp.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP; //ORing of all the flags 
+   nidApp.hIcon = hMainIcon; // handle of the Icon to be displayed, obtained from LoadIcon 
+   nidApp.uCallbackMessage = WM_USER_SHELLICON; 
+   LoadString(hInstance, IDS_APPTOOLTIP,nidApp.szTip,MAX_LOADSTRING);
+   Shell_NotifyIcon(NIM_ADD, &nidApp); 
+
+   return TRUE;
+}
+
+void Init()
+{
+	// user defined message that will be sent as the notification message to the Window Procedure 
+}
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window
+//  WM_DESTROY	- post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+    POINT lpClickPoint;
+
+	switch (message)
+	{
+
+	case WM_USER_SHELLICON: 
+		// systray msg callback 
+		switch(LOWORD(lParam)) 
+		{   
+			case WM_RBUTTONDOWN: 
+				UINT uFlag = MF_BYPOSITION|MF_STRING;
+				GetCursorPos(&lpClickPoint);
+				hPopMenu = CreatePopupMenu();
+				InsertMenu(hPopMenu,0xFFFFFFFF,MF_BYPOSITION|MF_STRING,IDM_ABOUT,_T("About"));
+				if ( bDisable == TRUE )
+				{
+					uFlag |= MF_GRAYED;
+				}
+				InsertMenu(hPopMenu,0xFFFFFFFF,uFlag,IDM_TEST2,_T("Test 2")); // Test 2
+				InsertMenu(hPopMenu,0xFFFFFFFF,uFlag,IDM_TEST1,_T("Test 1")); // Test 1				
+				InsertMenu(hPopMenu,0xFFFFFFFF,MF_SEPARATOR,IDM_SEP,_T("SEP"));				
+				if ( bDisable == TRUE )
+				{
+					InsertMenu(hPopMenu,0xFFFFFFFF,MF_BYPOSITION|MF_STRING,IDM_ENABLE,_T("Enable"));									
+				}
+				else 
+				{
+					InsertMenu(hPopMenu,0xFFFFFFFF,MF_BYPOSITION|MF_STRING,IDM_DISABLE,_T("Disable"));				
+				}
+				InsertMenu(hPopMenu,0xFFFFFFFF,MF_SEPARATOR,IDM_SEP,_T("SEP"));				
+				InsertMenu(hPopMenu,0xFFFFFFFF,MF_BYPOSITION|MF_STRING,IDM_EXIT,_T("Exit"));
+									
+				SetForegroundWindow(hWnd);
+				TrackPopupMenu(hPopMenu,TPM_LEFTALIGN|TPM_LEFTBUTTON|TPM_BOTTOMALIGN,lpClickPoint.x, lpClickPoint.y,0,hWnd,NULL);
+				return TRUE; 
+
+		}
+		break;
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+			case IDM_ABOUT:
+				DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+				break;
+			case IDM_TEST1:
+				MessageBox(NULL,_T("This is a test for menu Test 1"),_T("Test 1"),MB_OK);
+				break;
+			case IDM_TEST2:
+				MessageBox(NULL,_T("This is a test for menu Test 2"),_T("Test 2"),MB_OK);
+				break;
+			case IDM_DISABLE:
+				bDisable = TRUE;
+				break;
+			case IDM_ENABLE:
+				bDisable = FALSE;
+				break;
+			case IDM_EXIT:
+				Shell_NotifyIcon(NIM_DELETE,&nidApp);
+				DestroyWindow(hWnd);
+				break;
+			default:
+				return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+		/*
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code here...
+		EndPaint(hWnd, &ps);
+		break;*/
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
---
+++ src/SysTrayDemo/resource.h
@@ -0,0 +1,41 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by SysTrayDemo.rc
+//
+#define IDC_MYICON                      2
+#define IDD_SYSTRAYDEMO_DIALOG          102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDS_APPTOOLTIP                  104
+#define IDM_EXIT                        105
+#define IDI_SYSTRAYDEMO                 107
+#define IDI_SMALL                       108
+#define IDC_SYSTRAYDEMO                 109
+#define IDR_MAINFRAME                   128
+#define ID_S_                           32771
+#define ID_S_EXIT                       32772
+#define ID_SYSTRAYMENU_                 32773
+#define ID_SYSTRAYMENU_ENABLE           32774
+#define ID_SYSTRAYMENU_DISABLE          32775
+#define ID_SYSTRAYMENU_TEST1            32776
+#define ID_SYSTRAYMENU_TEST2            32777
+#define IDM_DISABLE                     32778
+#define IDM_ENABLE                      32779
+#define IDM_TEST2                       32780
+#define IDM_TEST1                       32781
+#define ID_SYSTRAYMENU_SEP              32782
+#define IDM_SEP                         32783
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32784
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
